eeee683b55b3e5dca3d978fa5669f51015a62714
Pavanyadav1234 <pavansn44@gmail.com>
2026-05-24 01:45:45 +0530

    clean page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,3 @@
-'use client'
-import { useEffect, useState } from 'react'
-
-export default function SplashPage() {
-  const [go, setGo] = useState(false)
-
-  export default function Page() {
+export default function Page() {
   return null
-}
-}
-
-  useEffect(() => {
-    if (go) {
-      window.location.href = '/welcome'
-    }
-  }, [go])
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
-      <h1 className="text-white text-3xl font-medium">WorldX</h1>
-      <p className="text-gray-500 text-sm mt-2">Mini App</p>
-    </div>
-  )
 }

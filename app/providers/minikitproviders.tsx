9d712c61a778748b47d358c76c39b33e48ab57c3
Pavanyadav1234 <pavansn44@gmail.com>
2026-05-24 01:26:53 +0530

    safe minikit install

--- app/providers/minikitproviders.tsx
+++ app/providers/minikitproviders.tsx
@@ -1,10 +1,17 @@
 'use client'
-import { MiniKit } from '@worldcoin/minikit-js'
 import { useEffect } from 'react'
 
 export function MiniKitProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    MiniKit.install()
+    const install = async () => {
+      try {
+        const { MiniKit } = await import('@worldcoin/minikit-js')
+        MiniKit.install(process.env.NEXT_PUBLIC_APP_ID)
+      } catch (e) {
+        console.log('MiniKit not available:', e)
+      }
+    }
+    install()
   }, [])
   return <>{children}</>
 }

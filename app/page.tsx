f79456774530e0cf1872680c43bf93a666e1d0bc
Pavanyadav1234 <pavansn44@gmail.com>
2026-05-24 01:29:46 +0530

    fix splash for world app webview

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 'use client'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 export default function SplashPage() {
-  const router = useRouter()
+  const [go, setGo] = useState(false)
+
   useEffect(() => {
-    const timer = setTimeout(() => router.push('/welcome'), 2000)
+    const timer = setTimeout(() => setGo(true), 2000)
     return () => clearTimeout(timer)
-  }, [router])
+  }, [])
+
+  useEffect(() => {
+    if (go) {
+      window.location.href = '/welcome'
+    }
+  }, [go])
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black">
       <h1 className="text-white text-3xl font-medium">WorldX</h1>
